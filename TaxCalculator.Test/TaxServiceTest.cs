8c1bbc39bf418734c9a83b03edf514d1bf09bdf6
ecmillan-dev <85765807+ecmillan-dev@users.noreply.github.com>
2021-06-13 18:54:07 -0400

    Got first version of unit tests working?

---
+++ TaxCalculator.Test/TaxServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxCalculator.Models;
+
+namespace TaxCalculator.Test
+{
+    public class TaxServiceTest : Interface.ITaxService
+    {
+        public Task<float> CalculateTaxesForOrder(OrderInformation order)
+        {
+            return Task.Factory.StartNew(() => (float)1.35); 
+        }
+
+        public Task<GeneralTaxRate> GetTaxRateForLocation(string zip)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TaxCalculator.Test/UnitTest1.cs
+++ TaxCalculator.Test/UnitTest1.cs
@@ -1,15 +1,22 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using NUnit.Framework;
 using System;
+using System.IO;
+using System.Threading.Tasks;
 using TaxCalculator.Implementation;
 using TaxCalculator.Interface;
 
 namespace TaxCalculator.Test
 {
+
     public class Tests
     {
+        private ITaxCalculator taxCalculator;
+        private IConfiguration configuration;
         private ITaxService taxService;
+        private Controllers.TaxServiceController taxServiceController;
         protected Mock<ITaxService> MockedTaxService => Mock.Get(taxService);
 
         private IServiceProvider ServicesProvider { get; set; }
@@ -17,40 +24,29 @@ namespace TaxCalculator.Test
         [SetUp]
         public void Setup()
         {
-            var services = new ServiceCollection();
-            services.AddTransient<IMatchRepository, MatchRepositoryStub>();
+            // initialize config (we can just ignore the logger for now)
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            configuration = builder.Build();
+            
+            // bind services
+            taxCalculator = new TaxJarAdapterService(configuration, null);
+            taxService = new Implementation.TaxService(taxCalculator, null);
 
-            var serviceProvider = services.BuildServiceProvider();
+            
+            // Use DI to get instances of IMatchService            
 
-            matchRepository = serviceProvider.GetService<IMatchRepository>();
-
-
-            //calc.Setup(async c =>
-            //{
-            //    Models.GeneralTaxRate generalTaxRate = await c.GetTaxRateForLocation("35758");
-            //    return generalTaxRate;
-            //}).Returns(new System.Threading.Tasks.Task<Models.GeneralTaxRate>());
         }
-        private static void ConfigureServices(IServiceCollection services)
-        {
-            services.AddScoped<ITaxService>();
-            // https://stackoverflow.com/a/67806752
-        }
+       
+
 
         [Test]
-        public void Test1()
+        public async Task TestCalculateTaxesForOrderService()
         {
-            string greeting = "Hello, get-testy.";
-            Assert.That(greeting, Is.EqualTo("Hello, get-testy."));
-            
-        }
+            // var taxService2 = ServicesProvider.GetService<ITaxService>();
 
-        [Test]
-        public void TestCalculateTaxesForOrder()
-        {
-            var calc = new Mock<ITaxCalculator>();
-            calc.0
-            
             var model = new Models.OrderInformation()
             {
                 from_country = "US",
@@ -88,17 +84,60 @@ namespace TaxCalculator.Test
                 discount = 0,
             };
 
-            var value = calc.Setup(c => c.CalculateTaxesForOrder(model).Result).Returns<float>(x => x);
-     //       _mock.Setup(x => x.DoSomething(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())
-     //.Returns((string a, string b, string c) => string.Concat(a, b, c));
-            Assert.AreEqual(value, (float)1.35);
-            //Assert.That(model.from_country, Is.EqualTo("US"));
+            // hardcoded from last successful test with that input data set
+            float expectedResult = (float)1.43;
+            var result = await taxService.CalculateTaxesForOrder(model);
+            Assert.AreEqual(expectedResult, result);
+            
+        }
 
-            // Assert.AreEqual("1725 Slough Avenue", customer.Street);
+        [Test]
+        public async Task TestCalculateTaxesForOrderCalculator()
+        {
+            // var taxService2 = ServicesProvider.GetService<ITaxService>();
 
-            // https://stackoverflow.com/questions/38605935/how-to-mock-a-service-with-moq-and-nunit-in-mvc-application-prevent-nulls
-            // https://stackoverflow.com/questions/37724738/how-to-unit-test-asp-net-core-application-with-constructor-dependency-injection
-            // expected result -  "amount_to_collect": 1.35,
+            var model = new Models.OrderInformation()
+            {
+                from_country = "US",
+                from_zip = "92093",
+                from_state = "CA",
+                from_city = "La Jolla",
+                from_street = "9500 Gilman Drive",
+                to_country = "US",
+                to_zip = "90002",
+                to_state = "CA",
+                to_city = "Los Angeles",
+                to_street = "1335 E 103rd St",
+                amount = 15,
+                shipping = (float)1.5,
+                nexus_addresses = new Models.NexusAddress[1],
+                line_items = new Models.LineItem[1],
+            };
+
+            // init the arrays
+            model.nexus_addresses[0] = new Models.NexusAddress()
+            {
+                id = "Main Location",
+                country = "US",
+                zip = "92093",
+                state = "CA",
+                city = "La Jolla",
+                street = "9500 Gilman Drive",
+            };
+            model.line_items[0] = new Models.LineItem()
+            {
+                id = "1",
+                quantity = 1,
+                product_tax_code = "20010",
+                unit_price = 15,
+                discount = 0,
+            };
+
+            // hardcoded from last successful test with that input data set
+            float expectedResult = (float)1.43;
+            var result = await taxCalculator.CalculateTaxesForOrder(model);
+            Assert.AreEqual(expectedResult, result);
+
         }
     }
 }
